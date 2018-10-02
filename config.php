139fe6499bc6e6002f358549da866fe619e46e9f
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-02 14:18:46 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-  define('APPLICATION_PATH','/application/');
-  define('HEADER',APPLICATION_PATH.'header/header.php');
-  define('FOOTER',APPLICATION_PATH.'footer/footer.php');
+  define('APPLICATION_PATH','/application');
+  define('HEADER',APPLICATION_PATH.'/header/header.php');
+  define('FOOTER',APPLICATION_PATH.'/footer/footer.php');
 ?>
