f2abcf1def8c348dce1206604cae27ff8289ca5f
Влад <ikader132@gmail.com>
2018-10-07 21:09:40 +0300

    New project.

--- application/sols/second.php
+++ application/sols/second.php
@@ -7,8 +7,8 @@
         $takts_num = $_POST['tatks'];
         $red_num = $_POST['red'];
 
-        $cache1 = 1;
-        $cache2 = 1;
+        $cache1 = $red_num;
+        $cache2 = $red_num;
         $cache3 = 0;
 
         for ($i = 2; $i <= $takts_num; $i++) {
@@ -17,7 +17,7 @@
             $cache1 = $cache2;
             $cache2 = $cache3;
         }
-        $result =  'За '. $takts_num .' тактов(-а) из '. $red_num .' было получено '. $cache3 * $red_num .' бактерий(-ии)';
+        $result =  'За '. $takts_num .' тактов(-а) из '. $red_num .' было получено '. $cache3 .' бактерий(-ии)';
 
         echo json_encode( $result, JSON_UNESCAPED_UNICODE);
     }
