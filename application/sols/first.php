4f895767fe9bb52bc5a31a22d8ec93d9ea6460f0
Влад <ikader132@gmail.com>
2018-10-07 15:40:44 +0300

    New project.

--- application/sols/first.php
+++ application/sols/first.php
@@ -1,19 +1,4 @@
 <?php
-    $month = array(
-        1 => 'Зима',
-        2 => 'Зима',
-        3 => 'Весна',
-        4 => 'Весна',
-        5 => 'Весна',
-        6 => 'Лето',
-        7 => 'Лето',
-        8 => 'Лето',
-        9 => 'Осень',
-        10 => 'Осень',
-        11 => 'Осень',
-        12 => 'Зима',
-    );
-    echo $_GET;
     firstFunc();
 
     function firstFunc(){
@@ -35,5 +20,5 @@
 
         $result =  "Выбранное время года: ".$month[$_GET['month']];
 
-        return json_encode(array('result'   =>  $result));
+        echo json_encode(array('result'   =>  $result));
     }
