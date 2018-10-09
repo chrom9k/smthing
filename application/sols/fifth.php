16e4f87f5bf83b6073836a5d822bcdcaa603fbeb
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-09 12:09:17 +0300

    Update fifth.php

--- application/sols/fifth.php
+++ application/sols/fifth.php
@@ -8,22 +8,17 @@
     $k = 1;
     $new_str = '';
 
-    $l = count($str);
-        echo $l."\n";
+    $l = strlen($str);
     for ($i = 0; $i < $l;) {
         for ($j = $i; $j + 1 <= $l; $j++) {
-            echo $str[$j]. " ". $str[$j+1]."\n";
             if ($str[$j] == $str[$j + 1]) {
                 $k++;
-                echo $k."\n";
             } else break;
         }
         if ($k > 1) {
             $new_str .= $str[$i] ."(". $k .")";
-            echo $str[$i] ."(". $k .")";
         } else {
             $new_str .= $str[$i];
-            echo $str[$i];
         }
         $i += $k;
         $k = 1;
