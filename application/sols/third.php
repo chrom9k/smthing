b12513dd343c7885ae958ffac7987b35dc5d8d09
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-09 12:20:34 +0300

    Update third.php

--- application/sols/third.php
+++ application/sols/third.php
@@ -2,18 +2,12 @@
     if (!empty($_GET)) {
         thirdFunc();
     }
-    echo 'Zashlo';
 
     function thirdFunc() {
         $N = $_GET['numb'];
         $n = (int)$N/2;
         $k = 1;
-        $arr = $arr[$N][$N];
-        for ($i = 0; $i < $N; $i++) {
-            for ($j = 0; $j < $N; $j++) {
-                $arr[$i][$j] = 0;
-            }
-        }
+        $arr = array_fill(0, $N, array_fill(0, $N, 0));
         
         $i = 0;
         $j = $N/2;
