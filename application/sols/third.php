c50dff54ded40deaa10ae833a7dbbd8db378febe
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-09 12:25:28 +0300

    Update third.php

--- application/sols/third.php
+++ application/sols/third.php
@@ -5,12 +5,11 @@
 
     function thirdFunc() {
         $N = $_GET['numb'];
-        $n = (int)$N/2;
         $k = 1;
         $arr = array_fill(0, $N, array_fill(0, $N, 0));
         
         $i = 0;
-        $j = $N/2;
+        $j = (int)$N/2;
         while ($k <= $N*$N) {
             if (($i == -1) && ($j == $N)) {
                 $i = 0;
