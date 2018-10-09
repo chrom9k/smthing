1861e1f23f3b6b864cc4e9d4bf09238290062d74
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-09 12:33:49 +0300

    Update third.php

--- application/sols/third.php
+++ application/sols/third.php
@@ -9,7 +9,7 @@
         $arr = array_fill(0, $N, array_fill(0, $N, 0));
         
         $i = 0;
-        $j = (int)$N/2;
+        $j = (int)($N/2);
         echo $j;
         
         echo json_encode($arr);
