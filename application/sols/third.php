2c2a1094a11116338ebe1de55b5c04a35200160a
chrom9k <43777790+chrom9k@users.noreply.github.com>
2018-10-09 12:23:22 +0300

    Update third.php

--- application/sols/third.php
+++ application/sols/third.php
@@ -9,7 +9,40 @@
         $k = 1;
         $arr = array_fill(0, $N, array_fill(0, $N, 0));
         
-        
+        $i = 0;
+        $j = $N/2;
+        while ($k <= $N*$N) {
+            if (($i == -1) && ($j == $N)) {
+                $i = 0;
+                $j = 0;
+                continue;
+            }
+            if ($i < 0) {
+                $i = $N - 1;
+                continue;
+            }
+            if ($i > $N - 1) {
+                $i = 0;
+                continue;
+            }
+            if ($j > $N - 1) {
+                $j = 0;
+                continue;
+            }
+            if ($j < 0) {
+                $j = $N - 1;
+                continue;
+            }
+            if ($arr[$i][$j] != 0) {
+                $i += 2;
+                $j--;
+                continue;
+            }
+            $arr[$i][$j] = $k++;
+            $i--;
+            $j++;
+            echo $k;
+        }
         
         echo json_encode($arr);
     }
