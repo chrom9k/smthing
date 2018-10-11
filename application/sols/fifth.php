6fb674e762e837e2aabd219d7f49877d1574adc9
Vlad <nexkader@gmail.com>
2018-10-11 15:12:30 +0300

    div

--- application/sols/fifth.php
+++ application/sols/fifth.php
@@ -23,8 +23,8 @@
             $i += $k;
             $k = 1;
         }
-        $new_str = $new_str;
+
         echo $new_str;
 
-        echo json_encode("Получившаяся строка:".  $new_str, JSON_UNESCAPED_UNICODE);
+//        echo json_encode("Получившаяся строка:".  $new_str, JSON_UNESCAPED_UNICODE);
     }
