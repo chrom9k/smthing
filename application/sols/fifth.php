0c0f1d1755c737bf2f82eec5640fc127463155d0
Vlad <nexkader@gmail.com>
2018-10-11 14:37:29 +0300

    div

--- application/sols/fifth.php
+++ application/sols/fifth.php
@@ -4,27 +4,26 @@
     }
 
     function fifthFunc() {
-    $str = $_POST['string'];
-    $k = 1;
-    $new_str = '';
-    echo $str;
-
-    $l = strlen($str);
-    for ($i = 0; $i < $l;) {
-        for ($j = $i; $j + 1 <= $l; $j++) {
-            if ($str[$j] == $str[$j + 1]) {
-                $k++;
-            } else break;
-        }
-        if ($k > 1) {
-            $new_str .= $str[$i] ."(". $k .")";
-        } else {
-            $new_str .= $str[$i];
-        }
-        $i += $k;
+        $str = $_POST['string'];
         $k = 1;
-    }
+        $new_str = '';
 
-    echo $new_str;
-    echo json_encode('Получившаяся строка: '. $new_str, JSON_UNESCAPED_UNICODE);
-}
+        $l = strlen($str);
+        for ($i = 0; $i < $l;) {
+            for ($j = $i; $j + 1 <= $l; $j++) {
+                if ($str[$j] == $str[$j + 1]) {
+                    $k++;
+                } else break;
+            }
+            if ($k > 1) {
+                $new_str .= $str[$i] ."(". $k .")";
+            } else {
+                $new_str .= $str[$i];
+            }
+            $i += $k;
+            $k = 1;
+        }
+
+        echo $new_str;
+        echo json_encode('Получившаяся строка: '. $new_str, JSON_UNESCAPED_UNICODE);
+    }
