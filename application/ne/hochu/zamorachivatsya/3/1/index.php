1205519852949f3983e5bfd38b2f7271bafa6ba0
Влад <ikader132@gmail.com>
2018-12-10 10:23:27 +0300

    New project.

--- application/ne/hochu/zamorachivatsya/3/1/index.php
+++ application/ne/hochu/zamorachivatsya/3/1/index.php
@@ -6,17 +6,17 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="windows-1251">
-	<title>Form</title>
-</head>
-<body>
-	<form action="validate.php" method="POST">
-		<div>ФИО: <input type="text" name="fio"></div>
-        <div>E-mail: <input type="text" name="email"></div>
-        <div>Телефон: <input type="text" name="phone"></div>
+    <head>
+        <meta charset="windows-1251">
+        <title>Form</title>
+    </head>
+    <body>
+        <form action="validate.php" method="POST">
+            <div>ФИО: <input type="text" name="fio"></div>
+            <div>E-mail: <input type="text" name="email"></div>
+            <div>Телефон: <input type="text" name="phone"></div>
 
-        <div><button type="submit">Send</button></div>
-	</form>
-</body>
+            <div><button type="submit">Send</button></div>
+        </form>
+    </body>
 </html>
