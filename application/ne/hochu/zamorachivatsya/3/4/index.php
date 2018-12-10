9d19b6759730b9ff2ff3e01839ab1ce9e55e6240
Влад <ikader132@gmail.com>
2018-12-10 10:27:35 +0300

    New project.

--- application/ne/hochu/zamorachivatsya/3/4/index.php
+++ application/ne/hochu/zamorachivatsya/3/4/index.php
@@ -6,22 +6,28 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="windows-1251">
-	<title>Form</title>
-</head>
-<body>
-	<form action="calc.php" method="POST">
-		Введите первое число: <input type="text" name="x"><br>
-		Введите второе число: <input type="text" name="y"><br>
-		Операция: <select name="operation">
-			<option disabled>Операция</option>
-			<option value="plus">+</option>
-			<option value="minus">-</option>
-			<option value="mul">*</option>
-			<option value="div">/</option>
-		</select><br>
-		<button type="submit">Отправить</button>
-	</form>
-</body>
+    <head>
+        <meta charset="windows-1251">
+        <title>Form</title>
+    </head>
+    <body>
+        <form action="calc.php" method="POST">
+            <div>Введите первое число: <input type="text" name="x"></div>
+            <div>Введите второе число: <input type="text" name="y"></div>
+            <div>Операция: <select name="operation">
+                <option disabled>Операция</option>
+                <option value="plus">+</option>
+                <option value="minus">-</option>
+                <option value="mul">*</option>
+                <option value="div">/</option>
+            </select></div>
+            <div><button type="submit">Отправить</button></div>
+        </form>
+    </body>
 </html>
+
+<style>
+    form div {
+        margin: 5px;
+    }
+</style>
